debdd720456e543ff93fdb2c81bb5cd79eeb4df3
Anteii <34139858+Anteii@users.noreply.github.com>
2021-02-12 20:01:49 +0400

    Beautified output; Added code comments;

--- Math-Optimization/Source.cpp
+++ Math-Optimization/Source.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 #include "LinearMethods.h"
 
 #define PI 3.14159265358979323846
+
 using namespace std;
 
+// Function to inspect
 double testFunc(double x) {
 	return sin(pow(x, 2)) + pow(x, -45) + x;
 }
 
 int main() {
 	double maxX, maxY;
-
+	/*
+	a - left point
+	b - right point
+	eps - precision
+	*/
 	double a = 2;
 	double b = 6;
+	double eps = 1e-9;
 
-	maxX = dichotomySearch(testFunc, a, b, 0.0001);
+	// to beautify output
+	cout << setprecision(7);
+	cout << "                          Xmax     Ymax" << endl;
+	
+	cout << "Dichotomy search       ";
+	maxX = dichotomySearch(testFunc, a, b, eps);
 	maxY = testFunc(maxX);
 	cout << maxX << " " << maxY << endl;
-	maxX = goldenSectionSearch(testFunc, a, b, 0.0001);
+
+	cout << "Golden section search  ";
+	maxX = goldenSectionSearch(testFunc, a, b, eps);
 	maxY = testFunc(maxX);
 	cout << maxX << " " << maxY << endl;
-	maxX = fibonacciSearch(testFunc, a, b, 0.0001);
+
+	cout << "Fibonacci search       ";
+	maxX = fibonacciSearch(testFunc, a, b, eps);
 	maxY = testFunc(maxX);
 	cout << maxX << " " << maxY << endl;
+
 	return 0;
 }
